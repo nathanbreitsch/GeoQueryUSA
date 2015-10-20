c3856eadf0e3638d4bbe5f28bcb51536e189065e
Nathan Breitsch <nathanbreitsch@gmail.com>
2015-10-20 00:52:01 -0400

    scala hello world

---
+++ PopulationQuery.scala
@@ -0,0 +1,9 @@
+object PopulationQuery {
+  def main(arguments: Array[String]){
+    val filename = arguments(0)
+    val numColumns = arguments(1)
+    val numRows = arguments(2)
+    val version = arguments(3)
+    println("Hey Dude!")
+  }
+}
